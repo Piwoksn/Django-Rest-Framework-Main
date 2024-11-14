35ebc7a2b69acb57075f00c5985219b954f9ba4d
Piwoksn <piwoksn@gmail.com>
2024-11-14 15:00:30 +0100

    used a dummy list api to render out

--- Post/urls.py
+++ Post/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 
 urlpatterns = [
-    path("home", views.homepage, name= "homepage/")
+    path("home/", views.homepage, name= "homepage"),
+    path("", views.list_post, name= "list_post"),
 ]
--- Post/views.py
+++ Post/views.py
@@ -14,7 +14,40 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.decorators import api_view
 
-@api_view(http_method_names=["GET"])
+posts = [
+    {
+        "id": 1,
+        "title": "Why is my name Noble Piwoks",
+        "content": "Discovering yourself"
+    },
+    {
+        "id": 2,
+        "title": "How is Life so far?",
+        "content": "In Gods Hands"
+    },
+    {
+        "id": 3,
+        "title": "What are your achievements",
+        "content": "Find out for yourself"
+    }
+]
+
+
+@api_view(http_method_names=["GET", "POST"])
 def homepage(request:Request):
-    response = {"message": "Hello World"}
+    
+    if request.method == 'POST':
+        data = request.data
+        response = response = [{"message": "Hello World"},
+                {"name": "Piwoks Noble"}, data]
+        return Response(data= response, status=status.HTTP_201_CREATED) 
+       
+    response = [{"message": "Hello World"},
+                {"name": "Piwoks Noble"}]
     return Response(data=response, status= status.HTTP_200_OK)
+
+
+@api_view(http_method_names=["GET"])
+def list_post(request:Request):
+    return Response(data=posts, status=status.HTTP_200_OK)
+
