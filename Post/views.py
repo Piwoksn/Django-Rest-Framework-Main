b2ddc305e8ca9a977f83a3726ec23bbb835d13a7
Piwoksn <piwoksn@gmail.com>
2024-11-14 13:31:51 +0100

    setup api JsonResonse for homepage

--- Post/views.py
+++ Post/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import JsonResponse, HttpRequest
 
 # Create your views here.
+
+def homepage(request:HttpRequest):
+    response = {
+        "message": "Hello World",
+    }
+    return JsonResponse(data=response)
