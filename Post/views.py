03e2d40e01caeaeba62d0b8488ef27df1afb6d04
Piwoksn <piwoksn@gmail.com>
2024-11-14 14:29:21 +0100

    USED REST_FRAME WORK API VIEW

--- Post/views.py
+++ Post/views.py
@@ -1,11 +1,20 @@
-# from django.shortcuts import render
-from django.http import JsonResponse, HttpRequest
-from rest_framework import response, request
+# # from django.shortcuts import render
+# from django.http import JsonResponse, HttpRequest
 
 
-def homepage(request:HttpRequest):
-    response = {
-        "message": "Hello World",
-    }
-    return JsonResponse(data=response)
+# def homepage(request:HttpRequest):
+#     response = {
+#         "message": "Hello World",
+#     }
+#     return JsonResponse(data=response)
 
+
+from rest_framework.request import Request
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.decorators import api_view
+
+@api_view(http_method_names=["GET"])
+def homepage(request:Request):
+    response = {"message": "Hello World"}
+    return Response(data=response, status= status.HTTP_200_OK)
