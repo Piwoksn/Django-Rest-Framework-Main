fc17a33fdafcca56908f77f7698a70a22f6f946f
Piwoksn <piwoksn@gmail.com>
2024-11-19 12:03:51 +0100

    class based view: api

--- Post/views.py
+++ Post/views.py
@@ -12,7 +12,7 @@
 from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework import status
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, APIView
 from .models import Post
 from .serializers import Modelserializer
 from django.shortcuts import get_object_or_404
@@ -116,4 +116,29 @@ def deletepost(request:Request, pk):
         "message": f"Deleted id = {pk}"
     }
     return Response(data=response, status=status.HTTP_200_OK)
+
+
+class PostGetClassView(APIView):
+    serialzer = Modelserializer
     
+    def get(self, request:Request, *args, **kwargs):
+        db = Post.objects.all()
+        
+        serialized = serializer(instance=db, many=True)
+        response = {
+            "message": "database",
+            "data": serialized.data
+        }
+        return Response(data=response, status=status.HTTP_200_OK)
+    
+    def post(self, request:Request, *args, **kwargs):
+        data = request.data
+        serialized = serializer(data=data)
+        
+        if serialized.is_valid():
+            serialized.save()
+            response = {
+                "message": "Created Successfully"
+            }
+            return Response(data=response, status=status.HTTP_201_CREATED)
+        return Response(data=serialized.errors, status=status.HTTP_400_BAD_REQUEST)
